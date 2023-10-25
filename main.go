18e5577f9fc63b3bc4683ef1286e4538f833f35a
Abdulrasheed Fawole <fawomath@gmail.com>
2023-10-25 23:03:05 +0100

    add dup1.go and dup2.go

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	ch1 "github.com/Abdulrasheed1729/gopl.bk/ch1"
 )
 
 func main() {
-	ch1.Echo1()
-	ch1.Echo2(os.Args)
-	ch1.Echo4(os.Args)
-	fmt.Println(ch1.Echo3(os.Args))
+	ch1.Dup2()
 }
